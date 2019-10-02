5a9fa6a41f3b1549d088854bae6b2df45699f960
Mark <mrigney@yozu.co.uk>
2019-10-02 16:10:11 +0100

    Completed - (Could deconstruct components...)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import "./App.css";
 
-function Todo({ todo, index }) {
+function Todo({ todo, index, completeTodo, deleteTodo }) {
 	return (
-		<div className="todo">
+		<div
+			style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+			className="todo"
+		>
 			<li>{todo.text}</li>
+			<div>
+				<button onClick={() => completeTodo(index)}>Mark as Complete</button>
+        <button onClick={() => deleteTodo(index)}>X</button>
+			</div>
 		</div>
 	);
 }
@@ -27,7 +34,7 @@ function TodoForm({ addTodo }) {
 				className="input"
 				value={value}
 				onChange={e => setValue(e.target.value)}
-        placeholder='Add New ToDo'
+				placeholder="Add New ToDo"
 			/>
 		</form>
 	);
@@ -54,11 +61,29 @@ function App() {
 		setTodos(NewTodos);
 	};
 
+	const deleteTodo = index => {
+		const newTodos = [...todos];
+    newTodos.splice(index, 1);
+		setTodos(newTodos);
+  };
+  
+  const completeTodo = index => {
+		const newTodos = [...todos];
+		newTodos[index].isCompleted = true;
+		setTodos(newTodos);
+	};
+
 	return (
 		<div className="app">
 			<div className="todo-list">
 				{todos.map((todo, index) => (
-					<Todo key={index} index={index} todo={todo} />
+					<Todo
+						key={index}
+						index={index}
+						todo={todo}
+						completeTodo={completeTodo}
+            deleteTodo={deleteTodo}
+					/>
 				))}
 				<TodoForm addTodo={addTodo} />
 			</div>
